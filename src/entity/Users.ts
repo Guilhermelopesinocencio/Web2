d024d53033396f9caf6569b8d3cd875e2ba988c9
Guilhermelopesinocencio <guilhermelopesinocencio@gmail.com>
2026-08-27 19:08:00 -0300

    Criação da Entity

---
+++ src/entity/Users.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+
+import { Situation } from "./Situations";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name!: string;
+
+    @Column({ unique: true })
+    email!: string;
+
+    @ManyToOne(() => Situation, (situation) => situation.users)
+    @JoinColumn({ name: "situation_id" })
+    situation!: Situation;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt!: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updatedAt!: Date;
+}
