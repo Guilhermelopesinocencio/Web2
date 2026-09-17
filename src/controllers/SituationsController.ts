51710d987ba55dec226f54ce0f5275153025519e
Guilhermelopesinocencio <guilhermelopesinocencio@gmail.com>
2026-09-17 18:00:39 -0300

    Criação do GET

--- src/controllers/SituationsController.ts
+++ src/controllers/SituationsController.ts
@@ -4,9 +4,47 @@ import { Situation } from "../entity/Situations";
 
 const router = express.Router();
 
-//Rota GET principal
-router.get("/situations", (req: Request, res: Response) => {
-    res.send("Bem vindo tela de Situations");
+//Criar Lista
+router.get("/situations", async (req: Request, res: Response) => {
+    try {
+        const situationRepository = AppDataSource.getRepository(Situation);
+        const situations = await situationRepository.find();
+
+        res.status(200).json(situations);
+        return;
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao buscar situações!",
+        });
+        return;
+    }
+});
+
+//Criar visualização do item cadastrado em situação
+router.get("/situations/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const situationRepository = AppDataSource.getRepository(Situation);
+        const situation = await situationRepository.findOneBy({ id: parseInt(id as string) });
+
+        if (!situation) {
+            res.status(404).json({
+                message: "Situação não encontrada!",
+            });
+            return;
+        }
+
+        res.status(200).json(situation);
+        return;
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao buscar situação!",
+        });
+        return;
+    }
 });
 
 //Rota POST
